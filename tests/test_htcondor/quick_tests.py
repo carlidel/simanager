c73a5be8463a783ea1c842aa40853df31f5ca3dc
Carlo Emilio MONTANARI <carlo.emilio.montanari@cern.ch>
2023-10-06 16:00:08 +0200

    remove reset

--- tests/test_htcondor/quick_tests.py
+++ tests/test_htcondor/quick_tests.py
@@ -15,12 +15,3 @@ sim.job_run_htcondor(study, time_limit="longlunch", request_gpus=False)
 
 # %%
 study.print_sim_status()
-
-# %%
-study.reset_simulations(
-    reset_all=True,
-    restore_original=True,
-    clear_out_folder=True,
-    clear_err_folder=True,
-    clear_log_folder=True,
-)
