a51c4b0d86a3cf3de097a1cc2731f2bd20f408ea
Carlo Emilio Montanari <carlidel95@gmail.com>
2023-10-11 15:33:43 +0200

    bump up version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="simanager",
-    version="0.0.2",
+    version="0.0.3",
     author="Carlo Emilio Montanari",
     author_email="carlo.emilio.montanari@cern.ch",
     description="A Python package for managing simulations locally, on HTCondor and on Slurm, with some specific elements that are good in a CERNy environment.",
