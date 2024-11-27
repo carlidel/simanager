0f57dd8fb56c2d1874fe57b6738fde1397476273
Carlo Emilio MONTANARI <carlo.emilio.montanari@cern.ch>
2024-11-27 15:21:50 +0100

    Add eos_stage_in_directory function to handle directory staging in eos_stage_in.py

--- simanager/templates/eos_stage_in/eos_stage_in.py
+++ simanager/templates/eos_stage_in/eos_stage_in.py
@@ -14,6 +14,20 @@ def eos_stage_in(eos_path: str, destination: str):
     return os.path.join(destination, basename)
 
 
+def eos_stage_in_directory(eos_path: str, destination: str):
+    basename = os.path.basename(eos_path)
+    destination = os.path.join(destination, basename)
+    # make sure both eos_path and destination end with a slash
+    if not eos_path.endswith("/"):
+        eos_path += "/"
+    if not destination.endswith("/"):
+        destination += "/"
+    os.makedirs(destination, exist_ok=True)
+    print(f"Copying {eos_path} to {destination}")
+    subprocess.run(["eos", "cp", "-r", eos_path, destination], check=True)
+    return destination
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--yaml_path", help="The path to the config file")
@@ -31,7 +45,14 @@ if __name__ == "__main__":
 
     # move the files
     for key, value in to_move_dict.items():
-        config[key] = eos_stage_in(value, "./input_eos")
+        # if value is a directory, use eos_stage_in_directory
+        # check if it is a directory properly
+        if os.path.isdir(value):
+            print(f"Staging in directory {value}")
+            config[key] = eos_stage_in_directory(value, "./input_eos")
+        else:
+            print(f"Staging in file {value}")
+            config[key] = eos_stage_in(value, "./input_eos")
 
     # write the new config file
     with open(args.yaml_path, "w", encoding="utf-8") as f:
